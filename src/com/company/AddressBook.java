c1663431b495fb8ad06eb8e2cdf2bc04a9219919
Sterling Jeppson <sjeppson@horizon.csueastbay.edu>
2021-02-19 23:19:46 -0800

    just nonsense to practice

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -39,6 +39,7 @@ public class AddressBook {
      * elements and ask user to select element based on index.
      */
     public void remove(String lastName) {
+        //I am not a crook
         TreeSet<AddressEntry> s = this.getPrefixSet(lastName);
         Scanner keyboard = new Scanner(System.in);
         try {
