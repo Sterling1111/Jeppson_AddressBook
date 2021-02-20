050f3a9aac648c6771df563e36176e5a2ba50764
Sterling Jeppson <Sterlingijeppson@gmail.com>
2021-02-20 11:53:39 -0800

    just added a comment

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -39,7 +39,7 @@ public class AddressBook {
      * elements and ask user to select element based on index.
      */
     public void remove(String lastName) {
-        //I am a crook
+        //How exactly does git work????
         TreeSet<AddressEntry> s = this.getPrefixSet(lastName);
         Scanner keyboard = new Scanner(System.in);
         try {
